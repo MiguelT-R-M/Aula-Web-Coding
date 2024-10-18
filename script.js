be59e39ec8519ecd5a3d7e584a36283ca042c9bf
MiguelT-R-M <161651029+MiguelT-R-M@users.noreply.github.com>
2024-10-18 12:15:12 +0000

    Aula dia 18/10

--- script.js
+++ script.js
@@ -1,16 +1,81 @@
+// tipos no javascript
+// number
+// string
+// boolean
+// undefined
+// object
+// function
+// declaração de variável
+// ecma6
+var nome = 'String';
+var nome2 = "String";
+var nome3 = `String`;
+console.log(typeof nome2);
+
+//
+let numero;
+let numero2 = 30;
+let numro3 = 30.5;
+console.log(typeof numero);
+
+// toda constante tem que ter valor, e não pode ser modificada
+const pi = 3.14;
+
+//
+
+let flag = false;
+
+//função
+
+function metodo(){
+
+    console.log("oi")
+
+}
+
+metodo();
+
+//objeto
+
+let user = {
+
+    nome: "carlos",
+    idade: 39,
+    endereço: {
+
+        rua: 3,
+        cidade: "cabo"
+
+    }
+
+}
+
+// crase
+
+let a = "nome";
+let b = 'nome';
+let c = `no
+me`;
+
+console.log(`O seu nome é ${nome}`);
+
+//
+
+function imprime(){
+
+    let guardanome = document.getElementById("nome").value;
+    console.log(`O seu nome é ${guardanome}`);
+    document.getElementById("imprime").innerHTML = ` O seu nome é ${guardanome}`;
+    document.getElementById("nome").value = "";
+
+}
+
+//
+
 function mudaCor(){
 
-    let texto = document.getElementById("cor").value
-    let texto2 = document.getElementById("corbloco").value
-
-    document.body.style.background = texto
-    
-    let cards = document.querySelectorAll(".quadrado")
-
-    cards.forEach(e =>{
-
-        e.style.background = texto2
-
-    })
+    let guardacor = document.getElementById("cor").value;
+    console.log(guardacor);
+    document.body.style.background = guardacor;
 
 }
